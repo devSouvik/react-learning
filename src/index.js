9e8ada7ec6663663410fa369f0ad50199f4310f3
devSouvik <souvikguria98@gmail.com>
2021-10-09 08:15:41 +0530

    book list started

--- src/index.js
+++ src/index.js
@@ -1,16 +1,60 @@
 import React from "react";
-import ReactDom from "react-dom";
+import ReactDOM from "react-dom";
 
-function Greetings() {
+function BookList() {
   return (
     <div>
-      <h1>hii all....</h1>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
     </div>
   );
 }
 
-const Greeting = () => {
-  return <div></div>;
-};
+function Book() {
+  return (
+    <div align="center">
+      <Image />
+      <BookName />
+      <Author />
+      <TimesSold />
+      <Price />
+    </div>
+  );
+}
 
-ReactDom.render(<Greetings />, document.getElementById("root"));
+function Image() {
+  return (
+    <div>
+      <img
+        src="https://images-eu.ssl-images-amazon.com/images/I/51T8OXMiB5L._SY264_BO1,204,203,200_QL40_FMwebp_.jpg"
+        alt="Book image"
+      />
+    </div>
+  );
+}
+
+function BookName() {
+  return <p>Ikigai: The Japanese secret to a long and happy life</p>;
+}
+
+function Author() {
+  return <h5>Hector Gracia</h5>;
+}
+
+function TimesSold() {
+  return <p>23,998 times sold</p>;
+}
+
+function Price() {
+  return <p>Rs. 230</p>;
+}
+
+// const Greeting = () => {
+//   return document, createElement("h1", {}, "hello world");
+// };
+
+ReactDOM.render(<BookList />, document.getElementById("root"));
