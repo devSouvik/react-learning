b48971eb990ccba8b4c7debdde1fbcdfbe571906
devSouvik <souvikguria98@gmail.com>
2021-10-12 10:47:27 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,60 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import "./style.css";
+
 function BookList() {
   return (
-    <div>
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+    <div className="bookList">
       <Book />
     </div>
   );
 }
 
 function Book() {
+  const price = "Rs. 230";
+  const title = "Ikigai: The Japanese secret to a long and happy life";
+  const author = "Hector Gracia";
+  const sold = "23,998 times sold";
   return (
-    <div align="center">
-      <Image />
-      <BookName />
-      <Author />
-      <TimesSold />
-      <Price />
-    </div>
-  );
-}
-
-function Image() {
-  return (
-    <div>
+    <div className="book">
       <img
+        id="image-id"
         src="https://images-eu.ssl-images-amazon.com/images/I/51T8OXMiB5L._SY264_BO1,204,203,200_QL40_FMwebp_.jpg"
         alt="Book image"
       />
+      <p>{title}</p>
+      <h5>{author}</h5>
+      <p>{sold}</p>
+      <p id="priceId">{price}</p>
     </div>
   );
 }
 
-function BookName() {
-  return <p>Ikigai: The Japanese secret to a long and happy life</p>;
-}
-
-function Author() {
-  return <h5>Hector Gracia</h5>;
-}
-
-function TimesSold() {
-  return <p>23,998 times sold</p>;
-}
-
-function Price() {
-  return <p>Rs. 230</p>;
-}
-
-// const Greeting = () => {
-//   return document, createElement("h1", {}, "hello world");
-// };
+// function NextPage() {
+//   return <a href="next.js">Click here</a>;
+// }
 
 ReactDOM.render(<BookList />, document.getElementById("root"));
