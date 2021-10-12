1b7dda15f3c0336f15f07e4bb1c599ee0300c6f8
devSouvik <souvikguria98@gmail.com>
2021-10-12 13:04:33 +0530

    props added

--- src/index.js
+++ src/index.js
@@ -3,36 +3,55 @@ import ReactDOM from "react-dom";
 
 import "./style.css";
 
+let firstBook = {
+  bookprice: "Rs. 230",
+  booktitle: "Ikigai: The Japanese secret to a long and happy life",
+  bookimg:
+    "https://m.media-amazon.com/images/I/81l3rZK4lnL._AC_UY327_FMwebp_QL65_.jpg",
+  bookauthor: "Hector Gracia",
+  booksold: "23,998 times sold",
+};
+
+const secondBook = {
+  bookprice: "Rs. 160",
+  booktitle: "The Psychology of Money",
+  bookimg:
+    "https://m.media-amazon.com/images/I/81cpDaCJJCL._AC_UY327_FMwebp_QL65_.jpg",
+  bookauthor: "Morgan Housel ",
+  booksold: "15,000 times sold",
+};
+
 function BookList() {
   return (
     <div className="bookList">
-      <Book />
+      <Book
+        price={firstBook.bookprice}
+        title={firstBook.booktitle}
+        img={firstBook.bookimg}
+        author={firstBook.bookauthor}
+        sold={firstBook.booksold}
+      />
+      <Book
+        price={secondBook.bookprice}
+        title={secondBook.booktitle}
+        img={secondBook.bookimg}
+        author={secondBook.bookauthor}
+        sold={secondBook.booksold}
+      />
     </div>
   );
 }
 
-function Book() {
-  const price = "Rs. 230";
-  const title = "Ikigai: The Japanese secret to a long and happy life";
-  const author = "Hector Gracia";
-  const sold = "23,998 times sold";
+function Book(props) {
   return (
     <div className="book">
-      <img
-        id="image-id"
-        src="https://images-eu.ssl-images-amazon.com/images/I/51T8OXMiB5L._SY264_BO1,204,203,200_QL40_FMwebp_.jpg"
-        alt="Book image"
-      />
-      <p>{title}</p>
-      <h5>{author}</h5>
-      <p>{sold}</p>
-      <p id="priceId">{price}</p>
+      <img id="image-id" src={props.img} alt="Book image" />
+      <p>{props.title}</p>
+      <h5>by {props.author}</h5>
+      <p>{props.sold}</p>
+      <p id="priceId">{props.price}</p>
     </div>
   );
 }
-
-// function NextPage() {
-//   return <a href="next.js">Click here</a>;
-// }
 
 ReactDOM.render(<BookList />, document.getElementById("root"));
